e4a53e8eeccd994d999a8ab1a4126ce6a97a664c
David Stein <stein.rs@gmail.com>
2022-06-29 09:10:50 -0300

    insert na tb fornecedor

--- conexao.py
+++ conexao.py
@@ -1,35 +1,37 @@
 import psycopg2
 
 # dados de configuração
-import query as query
-
 host = 'localhost'
 database = 'financeiro'
 user = 'postgres'
 password = 'postgres'
 # sslmode = 'require'
 
-# parametros
-conn_parametros = 'host={0} user={1} dbname={2} password={3}'.format(host, user, database, password)
+try:
+    # parametros
+    conn_parametros = 'host={0} user={1} dbname={2} password={3}'.format(host, user, database, password)
+    conn = psycopg2.connect(conn_parametros)
+    cursor = conn.cursor()
+except NameError as erro:
+    print('Erro', erro)
 
-conn = psycopg2.connect(conn_parametros)
-print('conectado com sucesso')
-cursor = conn.cursor()
 
-def inserir_fornecedor(args):
-    sql = "INSERT INTO tb_fornecedor(nome_forn) VALUES(%s);"
-    cursor.execute(sql,args)
-    conn.commit()
-    cursor.close()
-    conn.close()
+# def inserir_fornecedor(*args):
+#     sql = "INSERT INTO tb_fornecedor(nome_forn) VALUES(%s);"
+#     cursor.execute(sql,args)
+#     conn.commit()
+#     cursor.close()
+#     conn.close()
 
-def listar_fornecedor():
-    sql = "SELECT * FROM tb_fornecedor;"
-    cursor.execute(sql)
-    rows = cursor.fetchall()
+# def listar_fornecedor():
+#     sql = "SELECT * FROM tb_fornecedor;"
+#     cursor.execute(sql)
+#     rows = cursor.fetchall()
+#
+#     for row in rows:
+#         print('ID:', row[0], '- fornecedor: ', row[1])
 
-    for row in rows:
-        print('ID:', row[0], '- fornecedor: ', row[1])
 
-if (__name__ == '__main__'):
-    listar_fornecedor()
+
+# if (__name__ == '__main__'):
+#     listar_fornecedor()
