28916dddbbbd7cfdbe857ddd49d8afaad3657f69
David Stein <stein.rs@gmail.com>
2022-07-06 14:06:28 -0300

    Ajuste do codigo.

--- fornecedor.py
+++ fornecedor.py
@@ -6,15 +6,36 @@ def cadastro():
     print('************************')
     menu=0
     while(menu!=5):
-        menu = int(input('Informe uma das opções: \n 1 - Cadastrar\n 2 - Listar\n 3 - Atualizar'))
+        menu = int(input('Informe uma das opções: \n 1 - Cadastrar\n 2 - Listar\n 3 - Atualizar\n 4 - Excluir'))
+
         if(menu==1):
+            inserir()
+        elif(menu==2):
+            listar()
+        elif(menu == 3):
+            update()
+        elif(menu == 4):
             delete()
-        # elif(menu==2):
-        #     listar()
-        # elif(menu == 3):
-        #     update()
-        # elif(menu == 4):
-        #      delete()
+
+def inserir():
+        try:
+           print()
+           nome = input('Digite o nome do novo fornecedor; ')
+           sql = f"INSERT INTO tb_fornecedor(nome_forn) VALUES('{nome}');"
+           conexao.cursor.execute(sql)
+           conexao.conn.commit()
+           conexao.cursor.close()
+           conexao.conn.close()
+        except NameError as erro:
+            print('Erro', erro)
+
+def listar():
+    sql = "SELECT * FROM tb_fornecedor;"
+    conexao.cursor.execute(sql)
+    rows = conexao.cursor.fetchall()
+
+    for row in rows:
+        print('Cógido:', row[0], '- fornecedor: ', row[1])
 
 def update():
     try:
@@ -52,25 +73,6 @@ def delete():
     except NameError as erro:
         print('Erro', erro)
 
-def inserir():
-    try:
-        print()
-        nome = input('Digite o nome do novo fornecedor; ')
-        sql = f"INSERT INTO tb_fornecedor(nome_forn) VALUES('{nome}');"
-        conexao.cursor.execute(sql)
-        conexao.conn.commit()
-        conexao.cursor.close()
-        conexao.conn.close()
-    except NameError as erro:
-        print('Erro', erro)
-
-def listar():
-    sql = "SELECT * FROM tb_fornecedor;"
-    conexao.cursor.execute(sql)
-    rows = conexao.cursor.fetchall()
-
-    for row in rows:
-        print('Cógido:', row[0], '- fornecedor: ', row[1])
 #
 # if(__name__=='__main__'):
 #     cadastro()
