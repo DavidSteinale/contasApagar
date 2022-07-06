fa825e885216c568d4802f3254372cb2bf2e0317
David Stein <stein.rs@gmail.com>
2022-07-06 15:12:47 -0300

    Criação da classe TITULO

--- titulo.py
+++ titulo.py
@@ -9,13 +9,13 @@ def cadastro():
     print('*LANÇAMENTOS DE TITULOS*')
     print('************************')
     menu=0
-    listar()
+    update()
 
 def inserir():
     try:
         print()
         fornecedor = input('Digite o CODIGO do fornecedor? ')
-        valor = float(input('Divite o valor do titulo? '))
+        valor = float(input('Digite o valor do titulo? '))
         venc = input('Vencimento? ')
         sql = f"INSERT INTO tb_titulo(id_forn, venc, valor) VALUES('{fornecedor}','{venc}','{valor}');"
         conexao.cursor.execute(sql)
@@ -30,10 +30,29 @@ def listar():
         sql = "SELECT * FROM tb_titulo AS t INNER JOIN tb_fornecedor AS f ON t.id_forn=f.id_forn;"
         conexao.cursor.execute(sql)
         rows = conexao.cursor.fetchall()
+        print('Vencimento - Valor  - Fornecedor')
         for row in rows:
-            print('Vencimento:', row[2], '- valor: ', row[3], '- fonecedor: ', row[5])
+            print(row[2],'-', row[3],'-', row[5])
+            # print(f'Vencimento:', row[2], '- valor: ', row[3], '- fonecedor: ', row[5])
     except NameError as erro:
         print('Erro', erro)
+
+def update():
+    try:
+        id_titulo = int(input('Informe o código do titulo para alterar: '))
+        id_forn = input('Digite o código do fornecedor? ')
+        valor = float(input('Digite o valor do titulo? '))
+        venc = input('Vencimento? ')
+        sql = f"UPDATE tb_titulo SET id_forn={id_forn},venc='{venc}',valor={valor} WHERE id_titulo={id_titulo};"
+        conexao.cursor.execute(sql)
+        conexao.conn.commit()
+        conexao.cursor.close()
+        conexao.conn.close()
+    except NameError as erro:
+        print('Erro', erro)
+
+
+
 
 if(__name__=='__main__'):
      cadastro()
